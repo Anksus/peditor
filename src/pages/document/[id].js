c9750a39ec48a3bd44ff7d7458187201a07e1b42
Anksus <sankitpramod.min18@itbhu.ac.in>
2021-09-01 00:06:49 +0530

    redirect bug fix

--- src/pages/document/[id].js
+++ src/pages/document/[id].js
@@ -2,6 +2,7 @@ import { signIn, signOut, useSession } from "next-auth/client";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Welcome from "../components/welcome";
 
 const TextEditor = dynamic(() => import("../../components/textEditor"), {
   ssr: false,
@@ -10,14 +11,16 @@ const TextEditor = dynamic(() => import("../../components/textEditor"), {
 export default function Editor() {
   const router = useRouter();
   const [session, loading] = useSession();
-  useEffect(() => {
-    if (!session) {
-      router.push(`/`);
-    }
-  }, [session, router]);
+  // useEffect(() => {
+
+  //   if (!session) {
+  //     router.push(`/`);
+  //   }
+  // }, [session, router]);
   return (
     <div>
-      <TextEditor />
+      {!session && <Welcome />}
+      {session && <TextEditor />}
     </div>
   );
 }
